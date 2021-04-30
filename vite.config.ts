b78506c14df346000f4c21de09688537e04277a2
neurodynamic <>
2021-04-30 09:03:05 -0700

    starts working on parceljs switch

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import tauri from './tauri-plugin'
-import elmPlugin from 'vite-plugin-elm'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [elmPlugin(), tauri()]
-})
