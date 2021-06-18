db817fdc6d590ad227c0a4f8723fde7acdc80cd7
neurodynamic <>
2021-06-17 19:30:59 -0600

    removes all of the menu stuff (for now)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,26 +3,8 @@
   windows_subsystem = "windows"
 )]
 
-// use tauri::{CustomMenuItem, Menu, MenuItem};
-
 fn main() {
-  // let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-  // let menu = vec![
-  //   Menu::new("Debug Trainer", vec![
-  //     MenuItem::Custom(quit),
-  //   ])
-  // ];
-
   tauri::Builder::default()
-    // .menu(menu)
-    // .on_menu_event(|event| {
-    //   match event.menu_item_id().as_str() {
-    //     "quit" => {
-    //       std::process::exit(0);
-    //     }
-    //     _ => {}
-    //   }
-    // })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
